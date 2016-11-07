5aed5844e7a3b38b9fa8cdfa407d1b172ade81d9
Duncan McGreggor <oubiwann@gmail.com>
2016-11-07 14:04:12 -0600

    Updated deps.

--- project.clj
+++ project.clj
@@ -6,16 +6,16 @@
      :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies
     [[org.clojure/clojure "1.8.0"]
-     [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-     [org.clojure/clojurescript "0.0-3308"]
-     [cljsjs/react "0.13.3-1"]
-     [reagent "0.5.0"]
-     [reagent-forms "0.5.4"]
-     [io.nervous/fink-nottle "0.4.0"]
-     [io.nervous/cljs-lambda "0.1.2"]
+     [org.clojure/core.async "0.2.395"]
+     [org.clojure/clojurescript "1.9.293"]
+     [cljsjs/react "15.3.1-0"]
+     [reagent "0.6.0"]
+     [reagent-forms "0.5.28"]
+     [io.nervous/fink-nottle "0.4.6"]
+     [io.nervous/cljs-lambda "0.3.2"]
      [io.nervous/cljs-nodejs-externs "0.2.0"]
-     [jarohen/chord "0.6.0"]
-     [cljsjs/moment "2.9.0-0"]]
+     [jarohen/chord "0.7.0"]
+     [cljsjs/moment "2.15.2-2"]]
   :exclusions [[org.clojure/clojure]]
   :npm
     {:dependencies
@@ -28,9 +28,9 @@
    "resources/public/js/chemtrack.js"
    "target"]
   :plugins
-    [[lein-cljsbuild "1.0.6"]
-     [lein-npm "0.6.0"]
-     [io.nervous/lein-cljs-lambda "0.2.3"]]
+    [[lein-cljsbuild "1.1.4"]
+     [lein-npm "0.6.2"]
+     [io.nervous/lein-cljs-lambda "0.6.2"]]
   :figwheel {:open-file-command "emacsclient"}
   :cljs-lambda
     {:cljs-build-id "lambda"
@@ -71,4 +71,4 @@
          {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
           :dependencies
             [[com.cemerick/piggieback "0.2.1"]
-             [org.clojure/tools.nrepl "0.2.10"]]}})
+             [org.clojure/tools.nrepl "0.2.12"]]}})

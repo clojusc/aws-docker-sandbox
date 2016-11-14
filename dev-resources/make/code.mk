2317ac721b04e6a3e764e92b08c0b5550ffa59ad
Duncan McGreggor <oubiwann@gmail.com>
2016-11-14 10:19:47 -0600

    Updated make targets.

--- dev-resources/make/code.mk
+++ dev-resources/make/code.mk
@@ -1,12 +1,17 @@
 build:
 	lein compile
-	lein cljsbuild once
+	lein cljsbuild once frontend
+	lein cljsbuild once backend
+	lein cljsbuild once lambda
 
 lambda-deploy:
 	lein cljs-lambda deploy
 
 clean:
-	rm -rf pom.xml* target
+	lein clean
+	rm -rf pom.xml* node_modules
 
 run:
 	node target/cljs/backend/chemtrack.js
+
+run-fresh: clean build lambda-deploy run
